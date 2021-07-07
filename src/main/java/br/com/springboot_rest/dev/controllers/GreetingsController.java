644d4268ead9420622a41794ea7ca010a1643ea6
Diego Cardoso <diegocar448@hotmail.com>
2021-07-07 12:16:05 -0300

    Endpoint Rest/Api

--- src/main/java/br/com/springboot_rest/dev/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot_rest/dev/controllers/GreetingsController.java
@@ -1,12 +1,18 @@
 package br.com.springboot_rest.dev.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import br.com.springboot_rest.dev.model.Usuario;
 import br.com.springboot_rest.dev.repository.UsuarioRepository;
@@ -14,7 +20,7 @@ import br.com.springboot_rest.dev.repository.UsuarioRepository;
 /**
  *
  * A sample greetings controller to return greeting text
- */
+ */ 
 @RestController
 public class GreetingsController {
 	
@@ -44,5 +50,15 @@ public class GreetingsController {
     	
     	return "Ola fulano " + nome;
     	
+    }
+    
+    
+    @GetMapping(value = "listatodos") //tem a mesma função do @RequestMapping só que mais enxuto
+    @ResponseBody //Retorna os dados para o corpo da response JSON
+    public ResponseEntity<List<Usuario>> listaUsuario(){
+    	//findAll retorna uma lista de usuario
+    	List<Usuario> usuarios = usuarioRepository.findAll(); //executa a conulta no banco de dados
+    	
+    	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK); //Retornar a lista em JSON
     }
 }
