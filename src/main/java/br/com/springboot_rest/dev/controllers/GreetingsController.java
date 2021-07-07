8493d80e80478ed2d0c1b2c536337e0d9dc71d05
Diego Cardoso <diegocar448@hotmail.com>
2021-07-07 16:35:16 -0300

    Endpoint filter/update Users

--- src/main/java/br/com/springboot_rest/dev/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot_rest/dev/controllers/GreetingsController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -88,6 +89,35 @@ public class GreetingsController {
     	usuarioRepository.deleteById(iduser);
     	
     	return new ResponseEntity<String>("User deletado com sucesso", HttpStatus.OK);
+    	
+    }
+    
+    
+    /*Mapear a url na requisição verbo Get */
+    @GetMapping(value = "buscaruserid")
+    /*Retornará da descrição resposta */
+    @ResponseBody
+    public ResponseEntity<Usuario> buscaruserid(@RequestParam(name = "iduser") Long iduser){ //Recebe os dados para consultar
+    	Usuario usuario = usuarioRepository.findById(iduser).get();
+    	
+    	return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
+    	
+    }
+    
+    
+    /*Mapear a url na requisição verbo Post */
+    @PutMapping(value = "atualizar")
+    /*Retornará da descrição resposta */
+    @ResponseBody
+    public ResponseEntity<?> atualizar(@RequestBody Usuario usuario){ //Recebe os dados para salvar
+    	
+    	if(usuario.getId() == null) {
+    		return new ResponseEntity<String>("Id não foi informado.", HttpStatus.OK);
+    	}
+    	
+    	Usuario user = usuarioRepository.saveAndFlush(usuario); //atualizar
+    	
+    	return new ResponseEntity<Usuario>(user, HttpStatus.OK);
     	
     }
 }
